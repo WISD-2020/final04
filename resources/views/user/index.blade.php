1091ceb4634abb302ab4222da9772da014759dbe
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2021-01-08 15:26:25 +0800

    更改 <title>

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>lara_0901</title>
+    <title>個人資料 | final04</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
