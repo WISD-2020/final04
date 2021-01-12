6d712958328f6e99423b8c24976148c20375f02f
Wu Chen-En <ap9941002@gmail.com>
2021-01-12 18:15:57 +0800

    撰寫商品管理的路由

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,14 @@ Route::prefix('admin')->group(function () {
     Route::get('userlists', [AdminUserController::class, 'index'])->name('admin.userlists.index');                            //會員管理(顯示所有會員)
     Route::get('userlists/{id}/edit', [AdminUserController::class, 'edit'])->name('admin.userlists.edit');                    //編輯會員資料
     Route::patch('userlists/{id}',[AdminUserController::class, 'update'])->name('admin.userlists.update');                    //更新會員資料
-    Route::delete('userlists/{id}',[AdminUserController::class, 'destroy'])->name('admin.userlists.destroy');
+    Route::delete('userlists/{id}',[AdminUserController::class, 'destroy'])->name('admin.userlists.destroy');                 //刪除會員資料
+
+    Route::get('productlists', [AdminProductlistController::class, 'index'])->name('admin.productlists.index');                                  //商品管理(顯示所有商品)
+    Route::get('productlists/create', [AdminProductlistController::class, 'create'])->name('admin.productlists.create');                         //新增商品
+    Route::post('productlists/store',[AdminProductlistController::class, 'store'])->name('admin.productlists.store');                            //儲存商品
+    Route::get('productlists/{id}/edit', [AdminProductlistController::class, 'edit'])->name('admin.productlists.edit');                          //編輯商品
+    Route::patch('productlists/{id}',[AdminProductlistController::class, 'update'])->name('admin.productlists.update');                          //更新商品
+    Route::delete('productlists/{id}',[AdminProductlistController::class, 'destroy'])->name('admin.productlists.destroy');                       //刪除商品
 });
 
 
